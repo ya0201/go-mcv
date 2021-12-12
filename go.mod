f711cd6b3dc5cdbcc9d05f96eb09c4497b9fb837
ya0201 <you@example.com>
2021-12-12 14:13:09 +0900

    Add initial main.go

--- go.mod
+++ go.mod
@@ -1,3 +1,6 @@
-module go-mcv
+module github.com/ya0201/go-mcv
+
+replace github.com/ya0201/go-mcv/pkg/comment => ./pkg/comment
+replace github.com/ya0201/go-mcv/pkg/nozzle => ./pkg/nozzle
 
 go 1.17
--- pkg/nozzle/nozzle.go
+++ pkg/nozzle/nozzle.go
@@ -1,7 +1,30 @@
 package nozzle
 
-import 
+import (
+	"github.com/ya0201/go-mcv/pkg/comment"
+)
 
 type Nozzle interface {
-  Pump(chan Comment) error
+	Pump(chan<- comment.Comment) error
+}
+
+func HelloNozzle() *helloNozzle {
+	return &helloNozzle{}
+}
+
+type helloNozzle struct {
+}
+
+func (hn *helloNozzle) Pump() (<-chan comment.Comment, error) {
+	c := make(chan comment.Comment, 50)
+
+	comm := comment.Comment{
+		StreamingPlatform: "hoge",
+		Msg:               "hello, nozzle!",
+	}
+
+	c <- comm
+	close(c)
+
+	return c, nil
 }
