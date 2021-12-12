9c5196734a5e533879224b7654800f02d8ff5e3a
ya0201 <you@example.com>
2021-12-13 01:23:16 +0900

    Cobrize

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import "github.com/ya0201/go-mcv/cmd"
+
+func main() {
+	cmd.Execute()
+}
+
+
+// package main
+
+// import (
+//   "fmt"
+
+//   "github.com/ya0201/go-mcv/pkg/nozzle"
+// )
+
+// func main() {
+//   c, _ := nozzle.HelloNozzle().Pump()
+//   for c := range c {
+//     fmt.Println(c.Msg)
+//   }
+// }
+
