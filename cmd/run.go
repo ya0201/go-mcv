654b1e2ba8810037e8b6d1305aa196504dd14896
ya0201 <you@example.com>
2021-12-14 14:59:49 +0900

    update run.go

--- cmd/run.go
+++ cmd/run.go
@@ -4,20 +4,16 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+	"github.com/ya0201/go-mcv/pkg/nozzle"
 )
 
 // runCmd represents the run command
 var runCmd = &cobra.Command{
 	Use:   "run",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Runs go-mcv.",
+	Long:  `Runs go-mcv. Currently, twitch is only supported.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("run called")
+		run()
 	},
 }
 
@@ -33,4 +29,11 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// runCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func run() {
+	c, _ := nozzle.HelloNozzle().Pump()
+	for c := range c {
+		fmt.Println(c.Msg)
+	}
 }
